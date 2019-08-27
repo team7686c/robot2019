a3329688cf5b7aa0fe6b4d2540df1297ce6d41fe
team7686c <me@gisch.dev>
2019-08-27 16:48:30 -0500

    Changed line of message

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -4,9 +4,9 @@ void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
 	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
+		pros::lcd::set_text(3, "I was pressed!");
 	} else {
-		pros::lcd::clear_line(2);
+		pros::lcd::clear_line(3);
 	}
 }
 
