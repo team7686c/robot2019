76ef831f517d66fc5c5bd6595eb1a1f7d14bc42b
team7686c <me@gisch.dev>
2019-09-23 16:09:33 -0500

    Added arm feedback controller.

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -6,52 +6,8 @@
 Program ideas:
 - Have a match clock on the controller screen
 - Have a vibration alarm for 30 seconds and 5 seconds
-- Use a class to control robot state
-- make a better feedback loop system
 */
-//
-// class Robot;
-//
-// class DrivetrainController;
-// class FeedbackController;
-//
-//
-// class FeedbackController {
-// public:
-// 	virtual void measure(Robot* robot);
-// 	virtual void act(Robot* robot);
-// };
-//
-//
-// // use "pros::Motor Robot::*output" as a parameter for a generic motor controller
-//
-// class DrivetrainController: public FeedbackController {
-// 	int left_speed;
-// 	int right_speed;
-//
-// 	// pros::Motor& left_motor;
-// 	// pros::Motor& right_motor;
-// 	// pros::Controller& controller;
-//
-// public:
-//
-// 	DrivetrainController(){
-// 		this->left_speed = 0;
-// 		this->right_speed = 0;
-// 	}
-//
-// 	void measure(Robot* robot){
-// 		this->left_speed = robot->controller->get_analog(pros::controller_analog_e_t::E_CONTROLLER_ANALOG_LEFT_Y);
-// 		this->right_speed = robot->controller->get_analog(pros::controller_analog_e_t::E_CONTROLLER_ANALOG_RIGHT_Y);
-//
-// 		std::cout << "Speed: " << this->right_speed << std::endl;
-// 	}
-//
-// 	void act(Robot* robot){
-// 		robot->left_drive->move_velocity(this->left_speed);
-// 		robot->right_drive->move_velocity(this->right_speed);
-// 	}
-// };
+
 
 class FeedbackController {
 public:
@@ -75,6 +31,7 @@ public:
 	}
 };
 
+
 class RollerController: public FeedbackController {
 public:
 	int roller_speed;
@@ -86,6 +43,20 @@ public:
 	void act(RobotDeviceInterfaces* robot){
 		robot->left_roller_motor->move_velocity(this->roller_speed);
 		robot->right_roller_motor->move_velocity(this->roller_speed);
+	}
+};
+
+
+class ArmController: public FeedbackController {
+public:
+	int arm_speed;
+
+	void measure(pros::Controller *controller){
+		this->arm_speed = (controller->get_digital(DIGITAL_L1) - controller->get_digital(DIGITAL_L2)) * 50;
+	}
+
+	void act(RobotDeviceInterfaces *robot){
+		robot->arm_motor->move_velocity(this->arm_speed);
 	}
 };
 
@@ -108,38 +79,29 @@ public:
 void opcontrol(){
 	std::cout << "Driver control\n";
 
-	pros::delay(500);
-
 	RobotDeviceInterfaces *robot = global_robot;
 	pros::Controller *controller = global_controller;
 
-	std::cout << "Post init\n";
-
-	std::vector<FeedbackController*> feedbackControllers = {new DrivetrainController(), new RollerController()};
-
-	// int arm_speed;
-	int roller_speed;
+	std::vector<FeedbackController*> feedbackControllers = {
+		new DrivetrainController(),
+		new RollerController(),
+		// new ArmController(),
+	};
 
 	while (true) {
-
 		// Measure phase
 		std::cout << "Start measure phase\n";
 		for(int i = 0; i < feedbackControllers.size(); i++){
 			feedbackControllers[i]->measure(controller);
 		}
-		// arm_speed = (robot->controller->get_digital(DIGITAL_L1) - robot->controller->get_digital(DIGITAL_L2)) * 50;
 
 		// Act phase
 		std::cout << "Start act phase\n";
 		for(int i = 0; i < feedbackControllers.size(); i++){
 			feedbackControllers[i]->act(robot);
 		}
-		// arm.move_velocity(arm_speed);
-
 
 		// Wait for next cycle to save power
 		pros::delay(1000 / 50);
-
-		std::cout << "Finish loop\n";
 	}
 }
