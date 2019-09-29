93bc12560a634627fc85a2935fd77b167c14ec3a
team7686c <me@gisch.dev>
2019-09-29 14:47:38 -0500

    Switched tray motor to red cartrige

--- src/robot.cpp
+++ src/robot.cpp
@@ -18,7 +18,7 @@ RobotDeviceInterfaces::RobotDeviceInterfaces() {
     this->left_drive_motor = new pros::Motor(11, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
     this->right_drive_motor = new pros::Motor(20, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
     this->arm_motor = new pros::Motor(1, MOTOR_GEARSET_36, true, MOTOR_ENCODER_DEGREES);
-    this->tray_motor = new pros::Motor(19, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
+    this->tray_motor = new pros::Motor(19, MOTOR_GEARSET_36, true, MOTOR_ENCODER_DEGREES);
     this->left_roller_motor = new pros::Motor(2, MOTOR_GEARSET_18, true, MOTOR_ENCODER_DEGREES);
     this->right_roller_motor = new pros::Motor(9, MOTOR_GEARSET_18, false, MOTOR_ENCODER_DEGREES);
 }
